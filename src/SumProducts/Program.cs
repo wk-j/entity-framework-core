8572988ddcdd79ce917ccb15894ebadd36069865
wk-j <somnuk.wk@gmail.com>
2018-09-11 13:38:08 +0700

    Sum products

---
+++ src/SumProducts/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Collections.Generic;
+using DynamicTables;
+
+namespace SumProducts {
+
+    class Program {
+
+        static List<Result> Query(string connectionString) {
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            var sql = @"
+SELECT
+YEAR(LastUpdate) AS Year,
+SUM(IF(MONTH(LastUpdate)=1,Total,NULL)) AS A1,
+SUM(IF(MONTH(LastUpdate)=2,Total,NULL)) AS A2,
+SUM(IF(MONTH(LastUpdate)=3,Total,NULL)) AS A3,
+SUM(IF(MONTH(LastUpdate)=4,Total,NULL)) AS A4,
+SUM(IF(MONTH(LastUpdate)=5,Total,NULL)) AS A5,
+SUM(IF(MONTH(LastUpdate)=6,Total,NULL)) AS A6,
+SUM(IF(MONTH(LastUpdate)=7,Total,NULL)) AS A7,
+SUM(IF(MONTH(LastUpdate)=8,Total,NULL)) AS A8,
+SUM(IF(MONTH(LastUpdate)=9,Total,NULL)) AS A9,
+SUM(IF(MONTH(LastUpdate)=10,Total,NULL)) AS A10,
+SUM(IF(MONTH(LastUpdate)=11,Total,NULL)) AS A11,
+SUM(IF(MONTH(LastUpdate)=12,Total,NULL)) AS A12
+FROM Orders
+GROUP BY Year";
+
+            var rs = connection.Query<Result>(sql).ToList();
+            connection.Close();
+            return rs;
+        }
+
+        static void Insert(MyContext context) {
+            var range = Enumerable.Range(0, 10_000);
+            var random = new Random();
+            foreach (var item in range) {
+                var date = new DateTime(random.Next(2000, 2019), random.Next(1, 13), random.Next(1, 28));
+                var order = new Order {
+                    LastUpdate = date,
+                    Total = random.Next(100)
+                };
+                context.Add(order);
+            }
+            context.SaveChanges();
+        }
+
+        static void CreateTable(MyContext context) {
+            context.Database.EnsureCreated();
+
+            if (!context.Orders.Any()) {
+                Insert(context);
+            }
+        }
+
+        static void Main(string[] args) {
+            var connectionString = "Host=localhost;User=root;Password=1234;Database=A1234";
+            var collection = new ServiceCollection();
+
+            collection.AddDbContext<MyContext>(options => {
+                options.UseMySql(connectionString);
+            });
+
+            var provider = collection.BuildServiceProvider();
+            var context = provider.GetService<MyContext>();
+            CreateTable(context);
+
+            var rs = Query(connectionString);
+            DynamicTable.From(rs).Write();
+        }
+    }
+}
---
+++ src/SumProducts/Result.cs
@@ -0,0 +1,17 @@
+namespace SumProducts {
+    class Result {
+        public int Year { set; get; }
+        public int? A1 { set; get; }
+        public int? A2 { set; get; }
+        public int? A3 { set; get; }
+        public int? A4 { set; get; }
+        public int? A5 { set; get; }
+        public int? A6 { set; get; }
+        public int? A7 { set; get; }
+        public int? A8 { set; get; }
+        public int? A9 { set; get; }
+        public int? A10 { set; get; }
+        public int? A11 { set; get; }
+        public int? A12 { set; get; }
+    }
+}

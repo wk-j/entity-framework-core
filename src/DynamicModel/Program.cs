99149c195881f5ee4c55963541b188a0b38e45ed
wk-j <somnuk.wk@gmail.com>
2018-09-20 19:37:21 +0700

    Refactor

--- src/DynamicModel/Program.cs
+++ src/DynamicModel/Program.cs
@@ -1,78 +1,55 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
-using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Serilog;
-using System.Linq;
 
 namespace DynamicModel {
-    public class ModelBase {
-        [Key]
-        public int _Id { set; get; } = 1;
-    }
 
-    class Utility {
-        public static Type BuildClass() {
-            var src = @"
-                using DynamicModel;
-                using System;
-                class A: ModelBase {
-                    public string Name { set; get; } = ""Name"";
-                    public string LastName { set; get; } = ""LastName"";
-                }
-                return typeof(A);
-            ";
-
-            var reference = ScriptOptions.Default.WithReferences(Assembly.GetExecutingAssembly());
-            var script = CSharpScript.Create(src, reference);
-            var compile = script.Compile();
-            var a = script.RunAsync().Result.ReturnValue as Type;
-            return a;
-        }
-    }
-
-    class Context : DbContext {
-        public Context(DbContextOptions options) : base(options) {
-
+    class DynamicContext : DbContext {
+        private readonly ModelOptions modelOptions;
+        public DynamicContext(DbContextOptions options, ModelOptions modelOptions) : base(options) {
+            this.modelOptions = modelOptions;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            // var entity = modelBuilder.Entity<Student>();
-            var type = Utility.BuildClass();
+            var type = ModelUtility.GenerateModelType(modelOptions.ModelType, modelOptions.TypeName);
             var entity = modelBuilder.Entity(type);
-
-            var a = Activator.CreateInstance(type);
-            //type.GetProperty("Name").SetValue(a, "Name");
-            //type.GetProperty("LastName").SetValue(a, "LastName");
-
+            var ins = Activator.CreateInstance(type);
             entity.HasData(new[] {
-                a
+                ins
             });
         }
     }
 
     class Program {
         static void Main(string[] args) {
+
+            var obj = new {
+                A = 100,
+                B = 200,
+                C = DateTime.Now
+            };
+
+            var modelOptions = new ModelOptions {
+                ModelType = obj.GetType(),
+                TypeName = "MyModel"
+            };
+
             var collection = new ServiceCollection();
             collection.AddLogging(builder => {
                 builder.AddConsole();
             });
 
-            collection.AddDbContext<Context>(builder => {
+            collection.AddSingleton<ModelOptions>(modelOptions);
+            collection.AddDbContext<DynamicContext>(builder => {
                 var conn = "Host=localhost;User Id=postgres; Password=1234;Database=DynamicModel";
                 builder.UseNpgsql(conn);
             });
 
             var provider = collection.BuildServiceProvider();
-            var context = provider.GetService<Context>();
+            var context = provider.GetService<DynamicContext>();
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
-
-
         }
     }
 }
